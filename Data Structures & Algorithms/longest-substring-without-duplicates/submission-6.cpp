0df8d723a0f4d1735513f1462dfc309461c03ffd
Anar Dzhafarov <144374926+anardzhafarov@users.noreply.github.com>
2026-08-01 11:55:37 +0200

    Add: longest-substring-without-duplicates - submission-6

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> d;
+        int bleft = 0, bright = 0, cleft = 0, cright = 0;
+        if (s == "")
+            return 0;
+
+        for(int i = 0; i < s.size(); i++){
+            // if (s[i] == ' ')
+            //     d[' ']++;
+            if (!d.count(s[i])){
+                d[s[i]]++;
+                cright=i;
+                if(cright - cleft > bright - bleft) {
+                    bright = cright, bleft = cleft;
+                }
+            } else {
+                if(cright - cleft > bright - bleft) {
+                    bright = cright, bleft = cleft;
+                }
+                while(d.count(s[i])){
+                    d.erase(s[cleft++]);
+                }
+                d[s[i]]++;
+                cright=i;
+            }
+        }
+        return bright - bleft + 1;
+    }
+};
